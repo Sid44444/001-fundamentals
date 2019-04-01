5b9c851287ce754d2a8655552e2a111e89a645e1
Sarah Adlington <cheshire6655@icloud.com>
2019-04-01 12:06:02 +0100

    All ok

--- app/calculator.js
+++ app/calculator.js
@@ -3,5 +3,38 @@ function add(a, b) {
 }
 
 module.exports = {
-    add
-};
+    add,subtract,multiply,isOdd,isEven,divisibleBy,squareIt
+};//This is the only module that is read.
+
+function subtract(a, b) {
+    return a - b;
+}
+//module.exports = {
+    //subtract
+//};
+
+function multiply(a, b) {
+    return a * b;
+}
+
+
+function isOdd(num) { 
+var remainder = num % 2
+if (remainder ===1 ) return true;
+if (remainder ===0 ) return false;
+}
+function isEven(num) {
+    var remainder = num % 2
+    if (remainder ===1 ) return false;
+    if (remainder ===0 ) return true;
+}
+
+function divisibleBy(a,b) {
+    var divisible = a % b
+    if (divisible ===1 ) return false;
+    if (divisible ===0 ) return true;
+}
+
+function squareIt(number) {
+    return number * number;
+ }
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,32 @@
 const calculator = require('../app/calculator');
 
+
 test('adds 1 + 2 to equal 3', () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
+
+test('subtracts 4 - 1 to equal 3', () => {
+  expect(calculator.subtract(4, 1)).toBe(3);
+});
+
+test('multiply 1 * 2 to equal 2', () => {
+  expect(calculator.multiply(1, 2)).toBe(2);
+});
+
+test('isOdd returns true for 5', () => {
+  expect(calculator.isOdd(5)).toBe(true);
+});
+
+test('isEven returns true for 6', () => {
+  expect(calculator.isEven(6)).toBe(true);
+});
+
+test('divisibleBy returns true for even number',()=> {
+  expect(calculator.divisibleBy(8,4)).toBe (true);
+});
+
+test('squareIt returns number timed by itself',()=> {
+  expect(calculator.squareIt(3)).toBe (9);
+} 
+
+);
